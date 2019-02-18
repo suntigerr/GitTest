9da4eb68b5355c24aff458b12710cb1837dc5b5f
suntigerr <343195025@qq.com>
2019-02-18 17:07:13 +0800

    添加一个点击按钮

--- app/src/main/java/com/example/sun/gittest/MainActivity.java
+++ app/src/main/java/com/example/sun/gittest/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.sun.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtn = findViewById(R.id.btn);
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"点击一次",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
